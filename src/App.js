7bb0db3b0746e6d4b2d8c3b5636bd60eba135be9
panudeth7 <panudeth7@gmail.com>
2021-08-04 15:23:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// import Peer from 'peerjs';
+
+const App = () => {
+    // const peer = new Peer('1')
+
+    const localVideo = useRef(null)
+    const [startVideo, setStartVideo] = useState(false)
+
+    const startVideoCall = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+            console.log('Received local stream');
+            localVideo.current.srcObject = stream;
+            console.log({ localVideo })
+            setStartVideo(true)
+            // localStream = stream;
+            // callButton.disabled = false;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return <div className="App" >
+        {startVideo && <div>starting</div>}
+        <video ref={localVideo} id="localVideo" playsInline autoPlay></video>
+        <button onClick={startVideoCall}>start</button>
     </div>
-  );
 }
 
-export default App;
+export default App
